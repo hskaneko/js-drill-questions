e1060971610cffa7b580eb45e4bdf3691ec83da6
Heisuke Kaneko <heisuke.kaneko@lifematics.co.jp>
2020-11-02 22:28:27 +0900

    Chapter4 : Q4-08 / added an example of output

--- Chapter_4/Q4-08.js
+++ Chapter_4/Q4-08.js
@@ -1,3 +1,4 @@
 const points = [3, 12, 1, 9, 32]
 const totalPoint = points.reduce( (total, current) => /* ① */ + /* ② */, /* ③ */ )
-console.log(`${points./* ④ */('/* ⑤ */')}=${totalPoint}`)
+
+console.log(`${points./* ④ */('/* ⑤ */')}=${totalPoint}`) // 出力例 : 3+12+1+9+32=57
