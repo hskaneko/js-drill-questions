4dc14018e7ea3e3376c119b220f0871d3d7eb74a
Heisuke Kaneko <heisuke.kaneko@lifematics.co.jp>
2020-11-02 22:25:03 +0900

    Chapter4 : Q4-12 / collect spelling

--- Chapter_4/Q4-12.js
+++ Chapter_4/Q4-12.js
@@ -1,5 +1,5 @@
 const temperatures = [ 25, 28, 31, 33, 32 ]
 const [sapporo, sendai, tokyo, nagoya, osaka] = temperatures
 
-console.log(sendai) // 出力例 : /* ①1 */
+console.log(sendai) // 出力例 : /* ① */
 console.log(nagoya) // 出力例 : /* ② */
