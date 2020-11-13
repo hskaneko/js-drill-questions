6163b9d9ecb082c1dbf76c9dd1d8033195b9cc21
Heisuke Kaneko <heisuke.kaneko@lifematics.co.jp>
2020-11-13 22:07:29 +0900

    Chapter 8 : fixed server.js

--- Chapter_8/Q8-09/server.js
+++ Chapter_8/Q8-09/server.js
@@ -10,8 +10,7 @@ app.get("/api/weathers", (req, res) => {
 
 const port = 3000
 app.listen(port, () => {
-  console.log('Start : Web Server for Q8-8')
+  console.log('Start : Web Server for Q8-9')
   console.log(`[URL] http://localhost:${port}`)
   console.log("To stop this web server, press Ctrl-C")
 })
-
