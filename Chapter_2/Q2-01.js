7760d3830144562a15f4fb0bb8e7f406f36921a8
Heisuke Kaneko <heisuke.kaneko@lifematics.co.jp>
2022-04-22 20:05:59 +0900

    fixed the code of Q2-01

--- Chapter_2/Q2-01.js
+++ Chapter_2/Q2-01.js
@@ -2,6 +2,6 @@ function greetings() {
   const today = new Date()
   const hour = /* ① */.getHours()
   if (hour > 5 /* ② */ hour < 9) return "おはようございます"
-  /* ③ */ (hour /* ④ */ 18) return "こんにちは"
+  /* ③ */ (hour /* ④ */ 18 && hour >= 9) return "こんにちは"
   /* ⑤ */ return "こんばんは"
 }
